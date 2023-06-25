4dd9012c8bd54c025c0856fb650ae8d1e8bc02db
szonjaozsvart <szonjaozsvart@gmail.com>
2023-06-25 21:53:46 +0200

    trying out basic options

--- Program.cs
+++ Program.cs
@@ -1,2 +1,13 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+namespace WeatherApp;
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        var client = new HttpClient();
+        var response = await client.GetAsync("http://api.openweathermap.org/data/2.5/weather?q=Budapest&appid=my_api");
+        var content = await response.Content.ReadAsStringAsync();
+        Console.WriteLine(content);
+    }
+}
